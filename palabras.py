5ff6d3fb906fe472030fc121a1f483beffd613d5
HassanColio <79224410+HassanColio@users.noreply.github.com>
2021-02-17 13:11:06 -0800

    Add files via upload

---
+++ palabras.py
@@ -0,0 +1,8 @@
+texto = "La cieudad fin de buenos aires, es conocida como la paris de america"
+palabras=texto.split()
+i=0
+while i < len(palabras):
+      palabra=palabras[i]
+
+i += 1
+print ("Numero de palabras : ",i)
